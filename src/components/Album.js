c874234db81c37d6ed0d57d2da8d23937501c028
Deanna Swallow <swallowdeanna@gmail.com>
2018-01-23 16:24:03 -0700

    Next Button Functionality Added

--- src/components/Album.js
+++ src/components/Album.js
@@ -53,6 +53,14 @@ class Album extends Component {
     this.play(newSong);
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const nextIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
+    const nextSong = this.state.album.songs[nextIndex];
+    this.setSong(nextSong);
+    this.play(nextSong);
+  }
+
   render() {
     return (
       <section className="album">
@@ -92,6 +100,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
           />
       </section>
     );
